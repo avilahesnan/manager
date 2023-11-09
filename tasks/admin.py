7bf354111f85b50b0b578bcbb5a4eaaa997ec1f0
Hesnan Ávila <avilahesnan@outlook.com>
2023-11-09 17:49:09 -0300

    new file: models

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Category, Task
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    ...
--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=50)
+    subject = models.CharField(max_length=70)
+    description = models.TextField()
+    term = models.DateTimeField()
+    is_completed = models.BooleanField(default=False)
+    slug = models.SlugField()
+    cover = models.ImageField(upload_to='tasks/covers/%Y/%m/%d/')
+    days_to_alert = models.IntegerField(default=7)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True
+    )
+
+    def __str__(self):
+        return self.name
