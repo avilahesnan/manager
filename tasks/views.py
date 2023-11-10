48c7b226b511eef03be0c181a96f7aae3fbb9bf8
Hesnan Ávila <avilahesnan@outlook.com>
2023-11-10 13:45:38 -0300

    modified: images

--- tasks/views.py
+++ tasks/views.py
@@ -26,23 +26,9 @@ def index(request):
 
 
 def task(request, id):
-    all_tasks = Task.objects.all()
-    alert = datetime.now() + timedelta(days=7)
-    week = alert.strftime('%Y/%m/%d')
-
-    tasks = []
-    for task in all_tasks:
-        term = task.term
-        converted = term.strftime('%Y/%m/%d')
-        result = datetime.strptime(week, '%Y/%m/%d') - datetime.strptime(converted, '%Y/%m/%d')
-        task.days_to_alert -= result.days
-
-        if not task.is_completed and task.days_to_alert <= 7:
-            tasks.append(task)
-
-    tasks.sort(key=lambda x: x.days_to_alert)
+    task = Task.objects.get(id=id)
 
     return render(request, 'tasks/pages/task-view.html', context={
         'is_detail_page': True,
-        'tasks': tasks
+        'task': task
     })
